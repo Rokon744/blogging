7a359271ae63588edd613b7183b1f4c2f6ccd308
Rokon744 <roknujjamanripon@gmail.com>
2024-08-20 23:16:33 +0600

    some add

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import 'tailwindcss/tailwind.css'
 
 const outfit = Outfit({ subsets: ["latin"], weight:["400", "500", "600", "700"] });
 
